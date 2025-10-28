90327f46dc05b6c5995bed5ec7e0403769ce3465
benison2k <jeremybensonlastrollo@gmail.com>
2025-10-28 20:49:11 +0800

    (FEAT) added table pagination in pending
    - added table pagination nav header for pending residents tab

--- app/views/dashboard/review.php
+++ app/views/dashboard/review.php
@@ -7,7 +7,7 @@
 <?php $base_url = '/iCensus-ent/public'; ?>
 <link rel="stylesheet" href="<?= $base_url ?>/assets/css/style.css">
 <link rel="stylesheet" href="<?= $base_url ?>/assets/css/users.css">
-<link rel="stylesheet" href="<?= $base_url ?>/assets/css/residents.css">
+<link rel="stylesheet" href="<?= $base_url ?>/assets/css/residents_table.css">
 <link rel="stylesheet" href="<?= $base_url ?>/assets/css/modal.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
@@ -31,38 +31,40 @@
                 <p>There are no pending resident entries to review.</p>
             </div>
         <?php else: ?>
-            <div class="table-responsive">
-                <table class="user-table">
-                    <thead>
-                        <tr>
-                            <th>Full Name</th>
-                            <th>Age</th>
-                            <th>Gender</th>
-                            <th>Address</th>
-                            <th>Date Submitted</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach($pending_residents as $resident): ?>
-                        <tr>
-                            <td><?= htmlspecialchars($resident['first_name'] . ' ' . $resident['last_name']) ?></td>
-                            <td><?= htmlspecialchars($resident['age']) ?></td>
-                            <td><?= htmlspecialchars($resident['gender']) ?></td>
-                            <td><?= htmlspecialchars($resident['house_no'] . ' ' . $resident['street'] . ', Purok ' . $resident['purok']) ?></td>
-                            <td><?= date('M d, Y h:i A', strtotime($resident['created_at'])) ?></td>
-                            <td>
-                                <a href="<?= $base_url ?>/residents/approve?id=<?= $resident['id'] ?>" class="action-btn" title="Approve">
-                                    <span class="material-icons" style="color: green;">check</span>
-                                </a>
-                                <a href="<?= $base_url ?>/residents/reject?id=<?= $resident['id'] ?>" class="action-btn" title="Reject" onclick="return confirm('Are you sure you want to reject and delete this entry?');">
-                                    <span class="material-icons" style="color: red;">close</span>
-                                </a>
-                                </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+            <div class="table-container">
+                <div class="table-responsive">
+                    <table class="user-table resident-table">
+                        <thead>
+                            <tr>
+                                <th>Full Name</th>
+                                <th>Age</th>
+                                <th>Gender</th>
+                                <th>Address</th>
+                                <th>Date Submitted</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($pending_residents as $resident): ?>
+                            <tr>
+                                <td><?= htmlspecialchars($resident['first_name'] . ' ' . $resident['last_name']) ?></td>
+                                <td><?= htmlspecialchars($resident['age']) ?></td>
+                                <td><?= htmlspecialchars($resident['gender']) ?></td>
+                                <td><?= htmlspecialchars($resident['house_no'] . ' ' . $resident['street'] . ', Purok ' . $resident['purok']) ?></td>
+                                <td><?= date('M d, Y h:i A', strtotime($resident['created_at'])) ?></td>
+                                <td>
+                                    <a href="<?= $base_url ?>/residents/approve?id=<?= $resident['id'] ?>" class="action-btn" title="Approve">
+                                        <span class="material-icons" style="color: green;">check</span>
+                                    </a>
+                                    <a href="<?= $base_url ?>/residents/reject?id=<?= $resident['id'] ?>" class="action-btn" title="Reject" onclick="return confirm('Are you sure you want to reject and delete this entry?');">
+                                        <span class="material-icons" style="color: red;">close</span>
+                                    </a>
+                                    </td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         <?php endif; ?>
     </div>
